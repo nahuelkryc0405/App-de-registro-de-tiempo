c5b390bc2181ab3a45cce9b57132516f90c32b76
nkryc <nahuelkryc@gmail.com>
2026-01-12 17:43:16 -0300

    actualización temp

--- src/main.jsx
+++
@@ -1,7 +0,0 @@
-import { AppRegistry } from 'react-native-web'
-import App from './App'
-
-AppRegistry.registerComponent('App', () => App)
-AppRegistry.runApplication('App', {
-  rootTag: document.getElementById('root')
-})
